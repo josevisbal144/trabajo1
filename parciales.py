540587283c9ad2d394af9d83ec3ade2eb3fe33c4
josevisbal144 <josevisbal144@gmail.com>
2026-04-06 09:47:19 -0500

    Update parciales.py
    Added functionality to display and search for students.

--- parciales.py
+++ parciales.py
@@ -4,3 +4,15 @@ notas3=float(input("ingrese la nota del parcial 3: "))
 suma=notas1+notas2+notas3
 print(f"el promedio de tus notas es: {suma/3}")
 
+# Mostrar estudiantes
+    elif opcion == "2":
+        for estudiante in estudiantes:
+            print(estudiante)
+
+    # Buscar estudiante
+    elif opcion == "3":
+        buscar = input("Ingrese ID: ")
+
+        for estudiante in estudiantes:
+            if estudiante["id"] == buscar:
+                print(estudiante)
