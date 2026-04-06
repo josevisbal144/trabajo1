2b91181786f39c13f71134c84f7f498b4f6b8372
josevisbal144 <josevisbal144@gmail.com>
2026-04-06 09:52:00 -0500

    Update notas.py
    Added functionality to update and delete notes based on user input.

--- notas.py
+++ notas.py
@@ -4,3 +4,27 @@ nota3=float(input("ingrese la nota del parcial 3: "))
 notas=[nota1, nota2, nota3]
 promedio = sum(notas) / len(notas)
 print(promedio)
+
+# Actualizar notes
+    elif opcion == "4":
+        buscar = input("Ingrese ID: ")
+
+        for notes in notes:
+            if notes["id"] == buscar:
+                notes["nombre"] = input("Nuevo nombre: ")
+                notes["edad"] = int(input("Nueva edad: "))
+
+    # Eliminar notes
+    elif opcion == "5":
+        buscar = input("Ingrese ID: ")
+
+        for notes in notes:
+            if notes["id"] == buscar:
+                notes.remove(notes)
+
+    # Salir
+    elif opcion == "6":
+        print("Programa finalizado")
+
+    else:
+        print("Opción incorrecta")
